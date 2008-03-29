593b15097364b898257009d4e41721011f2d65f9
JTHotBox <JTHotBox@f427fdc5-e448-0410-9b95-ddf33fbbb69f>
2008-03-29 12:24:15 +0000

    Добавил новый валидатор для сравнение в диапазоне, исправил валидатор регулярных выражений, добавил файл хав ту, для понимания как пользоваться валидарами

--- validator.js
+++ validator.js
@@ -1,3 +1,4 @@
+//Initialization of arrays with data
 var validationGroupForms = new Array();
 var validadedForms = new Array();
 
@@ -7,6 +8,9 @@ var isValidInputs = new Array();
 
 var validatorErrorDiv = new Array();
 
+var validatorRegExExpression = new Array();
+
+//Add regist of validation group and form
 function addForm(formId, validationGroup)
 {
 document.forms[formId].setAttribute("onsubmit", "return false");
@@ -16,51 +20,139 @@ validadedForms[validadedForms.length] = formId;
 
 }
 
+//Regist regex validator
 function addValidatorRegEx(checkedInputId, errorDivId, regExStr, validationGroup)
 {
-   document.getElementById(checkedInputId).setAttribute("onblur", "checkInputRegEx('" + checkedInputId + "', '" + regExStr + "');");
+   document.getElementById(checkedInputId).setAttribute("onblur", "checkInputRegEx('" + checkedInputId + "');");
    
-   validationGroupInputs[checkedInputId] = validationGroup;
-   validadedInputs[validadedInputs.length] = checkedInputId;   
-   isValidInputs[checkedInputId] = false;
+   validatorRegExExpression[checkedInputId] = regExStr;
    
-   validatorErrorDiv[checkedInputId] = errorDivId;
+   registrateValidatorData(checkedInputId, errorDivId, validationGroup);
 }
 
+//Regist required field validator
 function addValidatorRequiredField(checkedInputId, errorDivId, validationGroup)
 {
    document.getElementById(checkedInputId).setAttribute("onblur", "checkInputRequiredField('" + checkedInputId + "');");
    
-   validationGroupInputs[checkedInputId] = validationGroup;
-   validadedInputs[validadedInputs.length] = checkedInputId;   
-   isValidInputs[checkedInputId] = false;
-   
-   validatorErrorDiv[checkedInputId] = errorDivId;
+   registrateValidatorData(checkedInputId, errorDivId, validationGroup);
 }
 
+//Regist compare validator
 function addValidatorCompare(checkedInputId1, checkedInputId2, errorDivId, validationGroup)
 {
    document.getElementById(checkedInputId1).setAttribute("onblur", "checkInputCompare('" + checkedInputId1 + "', '" + checkedInputId2 + "');");
    document.getElementById(checkedInputId2).setAttribute("onblur", "checkInputCompare('" + checkedInputId1 + "', '" + checkedInputId2 + "');");
       
-   validationGroupInputs[checkedInputId1] = validationGroup;
-   validadedInputs[validadedInputs.length] = checkedInputId1;   
-   isValidInputs[checkedInputId1] = false;
-   
-   validatorErrorDiv[checkedInputId1] = errorDivId;
-   
-   validationGroupInputs[checkedInputId2] = validationGroup;
-   validadedInputs[validadedInputs.length] = checkedInputId2;   
-   isValidInputs[checkedInputId2] = false;
-   
-   validatorErrorDiv[checkedInputId2] = errorDivId;
+   registrateValidatorData(checkedInputId1, errorDivId, validationGroup);
+   registrateValidatorData(checkedInputId2, errorDivId, validationGroup);
 }
 
+//Regist range validator
+function addValidatorRange(checkedInputId, minValue, maxValue, errorDivId, validationGroup)
+{
+   document.getElementById(checkedInputId).setAttribute("onblur", "checkInputRange('" + checkedInputId + "', '" + minValue + "', '" + maxValue + "');");
+      
+   registrateValidatorData(checkedInputId, errorDivId, validationGroup);
+}
+
+//Add submit button for validation group and form
 function addSubmitButton(sumbitId, validationGroup)
 {
    document.getElementById(sumbitId).setAttribute("onclick", "isFormValid('" + validationGroup + "');");
 }
 
+//Check regex validator
+function checkInputRegEx(checkedInputId)
+{
+
+    if(isValidRegEx(checkedInputId, validatorRegExExpression[checkedInputId]))
+        {
+            isValidInputs[checkedInputId] = true;
+            showErrorDiv(false, checkedInputId);
+        }
+    else
+        {
+            isValidInputs[checkedInputId] = false;
+            showErrorDiv(true, checkedInputId);
+        }
+}
+
+//Check required field
+function checkInputRequiredField(checkedInputId)
+{
+    var checkedElement = document.getElementById(checkedInputId);
+
+    if(checkedElement.value != "")
+        {
+            isValidInputs[checkedInputId] = true;
+            showErrorDiv(false, checkedInputId);
+        }
+    else
+        {
+            isValidInputs[checkedInputId] = false;
+            showErrorDiv(true, checkedInputId);
+        }
+}       
+
+//Check compare validator
+function checkInputCompare(checkedInputId1, checkedInputId2)
+{
+    var checkedElement1 = document.getElementById(checkedInputId1);
+    var checkedElement2 = document.getElementById(checkedInputId2);
+
+    if(checkedElement1.value == checkedElement2.value && checkedElement1.value != "")
+        {
+            isValidInputs[checkedInputId1] = true;
+            isValidInputs[checkedInputId2] = true;
+            showErrorDiv(false, checkedInputId1);
+        }
+    else
+        {
+            isValidInputs[checkedInputId1] = false;
+            isValidInputs[checkedInputId2] = false;
+            showErrorDiv(true, checkedInputId1);
+        }
+}        
+
+//Check range validator
+function checkInputRange(checkedInputId, minValue, maxValue)
+{
+    var checkedElement = document.getElementById(checkedInputId);
+
+    var min = parseInt(minValue);
+    var max = parseInt(maxValue);
+    var value = parseInt(checkedElement.value);
+    
+    if(!isNaN(min) && !isNaN(max) && !isNaN(value))
+        if(min <= value && max >= value)
+            {
+                isValidInputs[checkedInputId] = true;
+                showErrorDiv(false, checkedInputId);
+            }
+        else
+            {
+                isValidInputs[checkedInputId] = false;
+                showErrorDiv(true, checkedInputId);
+            }
+    else
+        {
+            isValidInputs[checkedInputId] = false;
+            showErrorDiv(true, checkedInputId);
+        }
+}    
+
+//Save validation data in arrays
+function registrateValidatorData(checkedInputId, errorDivId, validationGroup)
+{
+   validationGroupInputs[checkedInputId] = validationGroup;
+   validadedInputs[validadedInputs.length] = checkedInputId;   
+   isValidInputs[checkedInputId] = false;
+   
+   validatorErrorDiv[checkedInputId] = errorDivId;
+}
+
+//Chech valid of form
 function isFormValid(validationGroup)
 {
     var generalCountInValidationGroup = 0;
@@ -89,6 +181,7 @@ function isFormValid(validationGroup)
                 showErrorDiv(true, checkedInputId);
 }
 
+//Get validation group of form
 function getFormByValidationGroup(validationGroup)
 {
     var returnValue = "none";
@@ -103,56 +196,7 @@ function getFormByValidationGroup(validationGroup)
     return returnValue;
 }
 
-function checkInputRegEx(checkedInputId, regExStr)
-{
-
-    if(isValidRegEx(checkedInputId, regExStr))
-        {
-            isValidInputs[checkedInputId] = true;
-            showErrorDiv(false, checkedInputId);
-        }
-    else
-        {
-            isValidInputs[checkedInputId] = false;
-            showErrorDiv(true, checkedInputId);
-        }
-}
-        
-function checkInputRequiredField(checkedInputId)
-{
-    var checkedElement = document.getElementById(checkedInputId);
-
-    if(checkedElement.value != "")
-        {
-            isValidInputs[checkedInputId] = true;
-            showErrorDiv(false, checkedInputId);
-        }
-    else
-        {
-            isValidInputs[checkedInputId] = false;
-            showErrorDiv(true, checkedInputId);
-        }
-}       
-
-function checkInputCompare(checkedInputId1, checkedInputId2)
-{
-    var checkedElement1 = document.getElementById(checkedInputId1);
-    var checkedElement2 = document.getElementById(checkedInputId2);
-
-    if(checkedElement1.value == checkedElement2.value)
-        {
-            isValidInputs[checkedInputId1] = true;
-            isValidInputs[checkedInputId2] = true;
-            showErrorDiv(false, checkedInputId1);
-        }
-    else
-        {
-            isValidInputs[checkedInputId1] = false;
-            isValidInputs[checkedInputId2] = false;
-            showErrorDiv(true, checkedInputId1);
-        }
-}        
- 
+//Check regex
 function isValidRegEx(checkedInputId, regExStr)
 {
     var returnValue = false;
@@ -169,6 +213,7 @@ function isValidRegEx(checkedInputId, regExStr)
     return returnValue;
 }
 
+//Show error div
 function showErrorDiv(visible, checkedInputId)
 {
             var errorDivId = validatorErrorDiv[checkedInputId];
