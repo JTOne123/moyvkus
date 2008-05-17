5166d95c8c764b4a14bf6f29b71bf26f7e8f30a6
JTHotBox <JTHotBox@f427fdc5-e448-0410-9b95-ddf33fbbb69f>
2008-05-17 16:56:54 +0000

    дизайн для вью рецепта

--- system/application/views/view_recipe.php
+++ system/application/views/view_recipe.php
@@ -1,92 +1,106 @@
- <div>
-       <table>
+ <div class="MainDivProfile">
+       <table cellpadding="0" cellspacing="0" class="MainTableProfile">
            <tr>
-               <td colspan="2">
+               <td colspan="2" class="UserStatus">
                    {ViewRecipeTitle}
                </td>
            </tr>
-           <tr>
-               <td>
-                   <img src="{RecipeImgUrl}" />
-               </td>
-               <td>
-                   <table>
-                       <tr>
-                           <td colspan="2">
-                               <a href="{LinkToUserProfile}"><img src="{UserImgUrl}" /></a>
-                               <br>
-                               <a href="{LinkToUserProfile}">{NameOfAuthor}</a>
-                           </td>
-                       </tr>
-                       <tr>
-                           <td>
-                               {AddedDateLabel}:
-                           </td>
-                           <td>
-                               {AddedDateValue}
-                           </td>
-                       </tr>
-                       <tr>
-                           <td>
-                               {CategoryNameLabel}:
-                           </td>
-                           <td>
-                               {CategoryNameValue}
-                           </td>
-                       </tr>
-                       <tr>
-                           <td>
-                               {KitchenNameLabel}:
-                           </td>
-                           <td>
-                               {KitchenNameValue}
-                           </td>
-                       </tr>
-                       <tr>
-                           <td>
-                               {RatingLabel}:
-                           </td>
-                           <td>
-                               {RatingValue}
-                           </td>
-                       </tr>
-                       <tr>
-                           <td colspan="2">
-                               <table>
-                                   <tr>
-                                       <td>
-                                           <img src="{UpArrowImgUrl}" />
-                                       </td>
-                                       <td>
-                                           <img src="{DownArrowImgUrl}" />
-                                       </td>
-                                   </tr>
-                               </table>
-                           </td>
-                       </tr>
-                   </table>
-               </td>
-           </tr>
-           <tr>
-               <td>
-                   {IngredientsText}:
-               </td>
-               <td>
-                   {RecipeText}:
-               </td>
-           </tr>
-           <tr>
-               <td>
-                   {IngredientsValue}
-               </td>
-               <td>
-                   {RecipeValue}
-               </td>
-           </tr>
-           <tr>
-               <td colspan="2">
-                   {CommentsBuilder}
-               </td>
-           </tr>
-       </table>
+		   <tr>
+			<td class="Dialog Message">
+				<table>
+				   <tr>
+				         <td class="Headers">
+							{RecipePhoto}
+						 </td>
+						 <td class="Headers">
+							{MainData}
+						 </td>
+				   </tr>
+				   <tr>
+					   <td class="ViewRecipeTD">
+						   <img src="{RecipeImgUrl}" />
+					   </td>
+					   <td class="ViewRecipeTD">
+						   <table>
+							   <tr>
+								   <td colspan="2">
+									   <a href="{LinkToUserProfile}"><img src="{UserImgUrl}" /></a>
+									   <br>
+									   <a href="{LinkToUserProfile}">{NameOfAuthor}</a>
+								   </td>
+							   </tr>
+							   <tr>
+								   <td class="LabelText LableValueViewRecipe">
+									   {AddedDateLabel}:
+								   </td>
+								   <td class="LableValue">
+									   {AddedDateValue}
+								   </td>
+							   </tr>
+							   <tr>
+								   <td class="LabelText LableValueViewRecipe">
+									   {CategoryNameLabel}:
+								   </td>
+								   <td class="LableValue">
+									   {CategoryNameValue}
+								   </td>
+							   </tr>
+							   <tr>
+								   <td class="LabelText LableValueViewRecipe">
+									   {KitchenNameLabel}:
+								   </td>
+								   <td class="LableValue">
+									   {KitchenNameValue}
+								   </td>
+							   </tr>
+							   <tr>
+								   <td class="LabelText LableValueViewRecipe">
+									   {RatingLabel}:
+								   </td>
+								   <td class="LableValue">
+									   {RatingValue}
+								   </td>
+							   </tr>
+							   <tr>
+								   <td colspan="2">
+									   <table>
+										   <tr>
+											   <td>
+												   <img src="{UpArrowImgUrl}" />
+											   </td>
+											   <td>
+												   <img src="{DownArrowImgUrl}" />
+											   </td>
+										   </tr>
+									   </table>
+								   </td>
+							   </tr>
+						   </table>
+					   </td>
+				   </tr>
+				   <tr>
+					   <td class="Headers">
+						   {IngredientsText}:
+					   </td>
+					   <td class="Headers">
+						   {RecipeText}:
+					   </td>
+				   </tr>
+				   <tr>
+					   <td>
+						   {IngredientsValue}
+					   </td>
+					   <td>
+						   {RecipeValue}
+					   </td>
+				   </tr>
+				   <tr>
+					   <td colspan="2">
+						   {CommentsBuilder}
+					   </td>
+				   </tr>
+			   </table>
+			</td>
+		</tr>
+	</table>
    </div>
