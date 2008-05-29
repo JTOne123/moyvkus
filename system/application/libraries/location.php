fa024a373b1cec108f706978dc206bbc28aeaf94
JTHotBox <JTHotBox@f427fdc5-e448-0410-9b95-ddf33fbbb69f>
2008-05-29 19:15:44 +0000

    новый локейшен

--- system/application/libraries/location.php
+++ system/application/libraries/location.php
@@ -13,38 +13,49 @@ class Location {
 	/*
 		Получение города по его ID
 	*/
-		function GetCity($CityID)
+	function GetCity($CityID)
 	{
 		$query = $this->ci->db->query("SELECT * FROM city WHERE city_id = $CityID");
 		$row = $query->row();
 		return $row;
 	}
 	
-	function GetCitys($CityID)
+	function GetCitys($RegionID)
 	{
-		 $query = $this->ci->db->query("SELECT * FROM city WHERE region_id = $CityID");
-		 foreach ($query->result_array() as $row)
-		 {
-		 	$rows[$row['city_id']]=$row['name'];
-		 }
-		  return $rows;
+		$return_value = Array();
+		
+		$query = $this->ci->db->query("SELECT * FROM city WHERE region_id = $RegionID");
+		
+		foreach($query->result() as $row)
+		{
+			$city_id = $row->city_id;
+			$name = $row->name;
+			
+			$return_value[$city_id] = $name;
+		}
+		
+		return $return_value;
 	}
 	/*
 	Получение региона по его ID
 	*/
 	function GetRegions($CountryID)
 	{
-    	$query = $this->ci->db->query("SELECT * FROM region WHERE country_id = $CountryID");
-		 foreach ($query->result_array() as $row)
-		 {
-		 	$rows[$row['region_id']]=$row['name'];
-		 }
-		 
-		  return $rows;
+		$return_value = Array();
+		
+		$query = $this->ci->db->query("SELECT * FROM region WHERE country_id = $CountryID");
+		
+		foreach($query->result() as $row)
+		{
+			$region_id = $row->region_id;
+			$return_value[$region_id] = $row->name;
+		}
+		
+		return $return_value;
 	}
 	
 	
-		function GetRegion($RegionID)
+	function GetRegion($RegionID)
 	{
 		$query = $this->ci->db->query("SELECT * FROM region WHERE region_id = $RegionID");
 		$row = $query->row();
@@ -57,17 +68,24 @@ class Location {
 	*/
 	function GetCountryNames()
 	{
-		 $query = $this->ci->db->query("SELECT country_id, name FROM country");		
-		 foreach ($query->result_array() as $row)
-		 {
-		 	$rows[$row['country_id']]=$row['name'];
-		 }
-		    $rows['']='';
-		  return $rows;
+		$return_value = Array();
+		
+		$query = $this->ci->db->query("SELECT country_id, name FROM country");	
+		foreach($query->result() as $row)
+		{
+			$country_id = $row->country_id;
+			$name = $row->name;
+			
+			$return_value[$country_id] = $name;
+		}	
+		
+		$return_value['']='';
+		
+		return $return_value;
 	}
 	
 	
-		function GetCountry($CountryID)
+	function GetCountry($CountryID)
 	{
 		$query = $this->ci->db->query("SELECT * FROM country WHERE country_id = $CountryID");
 		$row = $query->row();
