5294ab586396067aa6d534f80590e0f98b17c36d
jthotbox <jthotbox@f427fdc5-e448-0410-9b95-ddf33fbbb69f>
2008-10-13 21:26:04 +0000

    перевел на английский

---
+++ en/system/application/views/main_tpl.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<!-- PageHeader START -->
+   {header}
+	<!-- PageHeader END -->
+</head>
+<body>
+	<table class="MainTable" cellpadding="0" cellspacing="0">
+		<tr>
+			<td align="center" class="ImageHeaderTD">
+				<table>
+					<tr>
+						<td class="LogoTD">
+							<a href="<?=base_url()?>profile"><img src="<?=base_url()?>images/top_logo.gif" class="HeaderLinks"/></a>
+						</td>
+						<td>
+							<img src="<?=base_url()?>images/top_center_header.gif"/>
+						</td>
+						<td>
+							<a href="<?=base_url()?>logout"><img src="<?=base_url()?>images/top_logout.gif" class="HeaderLinks"/></a>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		<!-- Top Sidebar START-->
+		<tr>
+			<td class="BodyTD">
+				{menu}
+			</td>
+		</tr>
+		<!-- Top Sidebar END-->
+		<!-- Left Sidebar START -->
+		<!-- Left Sidebar END -->
+		<!-- Page Body START -->
+		<tr>
+			<td class="BodyTD">
+				{body}
+			</td>
+		</tr>
+		<!-- Page Body END -->
+		<!-- Body Footer START-->
+		<!-- Body Footer END -->
+		<!-- Counters START-->
+		<!-- Counters END-->
+		<tr>
+			<td align="center" class="About">
+				<br/>
+				<br/>
+				(c)&nbsp;<a id="firstAuthorLink"></a>&nbsp;|&nbsp;<a id="secondAuthorLink"></a>&nbsp;2008
+			</td>
+		</tr>
+		<tr>
+			<td align="center" class="ImageBottomTD">
+				<img src="<?=base_url()?>images/bottom.gif" class="ImageBottom"/>	
+			</td>
+		</tr>
+	</table>
+</body>
+
+    <script language="javascript" type="text/javascript">
+
+    var firstAuthorLink = document.getElementById('firstAuthorLink');
+    var secondAuthorLink = document.getElementById('secondAuthorLink');
+
+    var DatsyukName = "Paul Datsyuk";
+    var DatsyukId = 2;
+
+    var VerbovskyName = "Alex Verbovsky";
+    var VerbovskyId = 1;
+
+    if(Math.random()<0.5)
+    {
+    	firstAuthorLink.innerHTML = DatsyukName;
+    	secondAuthorLink.innerHTML = VerbovskyName;
+
+    	firstAuthorLink.href = "<?=base_url()?>profile/id/" + DatsyukId;
+    	secondAuthorLink.href = "<?=base_url()?>profile/id/" + VerbovskyId;
+    }
+    else
+    {
+    	firstAuthorLink.innerHTML = VerbovskyName;
+    	secondAuthorLink.innerHTML = DatsyukName;
+
+    	firstAuthorLink.href = "<?=base_url()?>profile/id/" + VerbovskyId;
+    	secondAuthorLink.href = "<?=base_url()?>profile/id/" + DatsyukId;
+    }
+
+    </script>
+    
+    <noindex>
+    <div align="center">
+    <!--LiveInternet counter--><script type="text/javascript"><!--
+document.write("<a href='http://www.liveinternet.ru/click' "+
+"target=_blank><img src='http://counter.yadro.ru/hit?t26.6;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";"+Math.random()+
+"' alt='' title='LiveInternet: показано число посетителей за"+
+" сегодня' "+
+"border=0 width=88 height=15><\/a>")//--></script><!--/LiveInternet-->
+    </div>
+    </noindex>
+
+	
+</html>
---
+++ system/application/language/english/main_lang.php
@@ -0,0 +1,321 @@
+<?php 
+$lang['title'] = "Oskrer"; 
+$lang['description'] = ""; 
+$lang['keywords'] = ""; 
+
+//Registration form 
+$lang['sign_up_message'] = "Registration of a new user"; 
+$lang['sign_up_slogan_message'] = "It will take less than 1 minute"; 
+$lang['first_name'] = "First name:"; 
+$lang['last_name'] = "Last name:"; 
+$lang['password'] = "Password:"; 
+$lang['repassword'] = "Password again:"; 
+$lang['email'] = "E-mail:"; 
+$lang['sign_up'] = "Sign up"; 
+$lang['captcha'] = "Code on a picture"; 
+
+//Authorization form 
+$lang['log_in'] = "Log in"; 
+$lang['forgot_password'] = "Forgot password?"; 
+$lang['checkbox_remember'] = "Remember me"; 
+$lang['login_text'] = "To log in the system authorization is required:"; 
+
+
+
+//Message from validators 
+$lang['Error_email'] = "E-mail is not correct"; 
+$lang['Error_firstname'] = "Name should be at least 4 characters long"; 
+$lang['Error_lastname'] = "Last name should be at least 4 characters long"; 
+$lang['Error_password'] = " Password should be at least 6 characters long"; 
+$lang['Error_repassword'] = "Passwords do not coincide"; 
+$lang['Error_captcha'] = "Code from the picture should be 4 characters long"; 
+$lang['captcha_check'] = "<div class = \"Registraion_validator\" style=\"display:block;\">Code from the picture is not correct! Try once more</div>"; 
+$lang['email_check'] = "<div class = \"Registraion_validator\" style=\"display:block;\">User with such E-mail has been already registrated!</div>"; 
+//Authorization form 
+$lang['check_user_mail_exist'] = "<div class = \"Registraion_validator MessageWide\" style=\"display:block;\">There is no user with such E-mail!</div>"; 
+$lang['check_password'] = "<div class = \"Registraion_validator\" style=\"display:block;\">Incorrect login or password.</div>"; 
+
+
+//Notification 
+$lang['AfterRegistraionEmailFrom'] = "support@Oskrer.com"; 
+$lang['AfterRegistraionEmailFromName'] = "Oskrer - Support"; 
+$lang['AfterRegistraionEmailSubject'] = "Registration of an account"; 
+$lang['AfterRegistraionEmailMessage'] = " 
+Dear 
+{first_name} {last_name} 
+Your account 
+has been activated 
+Your password: {password} 
+Best regards Oskrer.com administration http://www.Oskrer.com"; 
+
+$lang['InviteEmailSubject'] = "We invite you to register on our Oskrer.com project http://www.Oskrer.com"; 
+$lang['InviteEmailMessage'] = "Dear {InvetedUserFullName}<br/> 
+
+Oskrer.com user (http://www.moyvkus.ru), {UserFullName} invites you to take part in the project, dedicated to the tasty cuisines. 
+<br/> 
+To begin registration follow this link {UrlForRegister} 
+<br/> 
+If you are not eager to participate in the project or do not know this user, just delete this letter. 
+<br/> 
+The administration of the project Oskrer.com http://www.Oskrer.com"; 
+
+$lang['NewPasswordRequestMessage'] = "Dear user {UserFullName}, 
+Your new password to log in the system: {NewPassword} 
+The administration of the project Oskrer.com http://www.Oskrer.com"; 
+$lang['NewPasswordRequestSubject'] = "Your new password"; 
+
+
+$lang['NewMessageSubject']="User {UserFrom}has left a new private message for You"; 
+$lang['NewMessageText']="{ToUserFullName}, 
+You have got new private message. 
+You can read Your private messages on the page: 
+The administration of the project Oskrer.com http://www.Oskrer.com"; 
+
+$lang['NewFriendSubject']="User {UserFrom} wants to add You to his\her friendlist"; 
+$lang['NewFriendText']="{ToUserFullName}, 
+You have got new request for friendship. 
+You can look at your friendlist and find new friends on this page: 
+http://www.moyvkus.ru/myfriends 
+The administration of the project Oskrer.com http://www.Oskrer.com"; 
+
+$lang['NewCommentSubject']="User {UserFrom} left a comment to your recipe"; 
+$lang['NewCommentText']="{ToUserFullName}, 
+Your recipe was commented. 
+You can look at the recipe and read a comment here: 
+http://www.moyvkus.ru/view_recipe/id/{RecipeID} 
+The administration of the project Oskrer.com http://www.Oskrer.com"; 
+
+
+
+//Profile 
+$lang['Prifile'] = "Profile"; 
+$lang['Edit'] = "Edit"; 
+$lang['Avatar'] = "Avatar"; 
+$lang['MyFriendsHeader'] = "Functions"; 
+$lang['MyData'] = "Private information"; 
+$lang['FirstNameText'] = "First name:"; 
+$lang['LastNameText'] = "Last name:"; 
+$lang['SexText'] = "Sex:"; 
+$lang['BirthdayText'] = "Date of birth:"; 
+$lang['LoctionText'] = "Location:"; 
+$lang['WebSiteText'] = "web-site:"; 
+$lang['InstantMessagerText'] = "Phone number:"; 
+$lang['ActivitiesText'] = "Activity:"; 
+$lang['InterestsText'] = "Interests:"; 
+$lang['AboutText'] = "About me:"; 
+$lang['MyRatingText'] = "My rate:"; 
+$lang['MyRatingLevelText'] = "Rate:"; 
+$lang['MyBestRecipesText'] = "The best recipe:"; 
+$lang['MyRecipes'] = "My recipes"; 
+$lang['MyInfo'] = "About me"; 
+$lang['Contacts'] = "Contact information"; 
+$lang['MyRatingTextHeader'] = "Rates"; 
+$lang['Man'] = "Male"; 
+$lang['Woman'] = "Female"; 
+$lang['AddToFriends'] = "Add to friends"; 
+$lang['DeleteFromFriends'] = "Delete from friends"; 
+$lang['Friends'] = "Look through friendlist"; 
+$lang['NoRecipes'] = "No recipes added"; 
+$lang['HisRecipes'] = "His (her) recipes"; 
+$lang['RecomendedRecipes'] = "Probably, these recipes will be of Your interest"; 
+
+
+//Edit profile 
+$lang['EditProfile'] = "Edit profile"; 
+$lang['LoginInformation'] = "Registration information"; 
+$lang['NewEmailText'] = "E-mail:"; 
+$lang['OldPasswordText'] = "Previous password:"; 
+$lang['NewPassword'] = "New password:"; 
+$lang['ReNewPassword'] = "New password once more:"; 
+$lang['SaveAllChanges'] = "Save all changes?"; 
+$lang['Save'] = "Save"; 
+$lang['Upload'] = "Upload"; 
+$lang['Day'] = "Day"; 
+$lang['Month'] = "Month"; 
+$lang['Year'] = "Year"; 
+$lang['Country'] = "Country"; 
+$lang['City'] = "City"; 
+$lang['MySettings'] = "My settings"; 
+$lang['Cancel'] = "Cancel"; 
+$lang['Region'] = "Region"; 
+$lang['txtNewPassword'] = " Password should be at least 6 characters long"; 
+$lang['ErrorNewPassword'] = "<div class = \"Registraion_validator\" style=\"display:block;\">Passwords you entered are incorrect</div><br/>"; 
+
+//My friends 
+$lang['MyFriends'] = "My friends"; 
+$lang['FriendsFilter'] = "Friends filter"; 
+$lang['Search'] = "Search"; 
+$lang['MyFriendsCount'] = "You have {Number} friends"; 
+$lang['MyFriendsCountNew'] = "You have {Number} friends <span style=\"color:red;\"> + {NewFriendsNumber} new</span>"; 
+
+$lang['SendMessage'] = "Send message"; 
+$lang['FriendFriends'] = "His (her) friends"; 
+$lang['DeleteFriend'] = "Delete from friends"; 
+
+//MessageBox 
+$lang['MessageBoxTitleFriendDelete'] = "Delete from friends"; 
+$lang['MessageBoxTitle'] = "Delete from frienslist"; 
+$lang['MessageBoxText'] = 'Are you sure you want to delete participant<a href="{FriendUrl}">{FriendFullName}</a> from your friendlist?'; 
+
+$lang['MessageBoxTextAddFriend'] = 'Are you sure you want to add participant <a href="{FriendUrl}">{FriendFullName}</a> to your friendlist?'; 
+
+$lang['Yes'] = "Delete"; 
+$lang['No'] = "Cancel"; 
+$lang['SpamWarning'] = "You are not able to send more than 20 messages per day to the users that are not in your friendlist"; 
+$lang['MessageBoxTitleSpamWarrning'] = "Attention"; 
+
+$lang['NewPasswordTitle'] = "New password"; 
+$lang['NewPasswordInformation'] = "New password was send to your E-mail"; 
+$lang['Ok'] = "Ok"; 
+
+//Invite 
+$lang['Invite'] = " Invite a friend"; 
+$lang['Information'] = "Information about your friend "; 
+$lang['Note'] = "Who can I invite ?"; 
+$lang['NoteAnswer'] = "You can invite only those people you know. Spam is forbidden."; 
+$lang['Email'] = "E-mail:*"; 
+$lang['FirstName'] = "First name:"; 
+$lang['LastName'] = "Last name:"; 
+$lang['Send'] = "Send invitation"; 
+
+//Send Message 
+$lang['send_message'] = "New message"; 
+$lang['From'] = "From:"; 
+$lang['To'] = "To:"; 
+$lang['Subject'] = "Subject:"; 
+$lang['Text'] = "Text:"; 
+$lang['Previous'] = "Previous message"; 
+
+//MyMessages 
+$lang['MyMessages'] = "My messages"; 
+$lang['MessagesFilter'] = "Messages’ filter"; 
+$lang['MyMessagesCount'] = "You have {Number} messages"; 
+$lang['Sender'] = "Sender"; 
+$lang['Action'] = "Options"; 
+$lang['Answer'] = "Answer"; 
+$lang['Delete'] = "Delete"; 
+$lang['MessageSubject'] = "Subject"; 
+
+//Get Message 
+$lang['GetMessage'] = "My messages"; 
+
+
+//Add recipe 
+$lang['AddRecipe'] = "Add recipe"; 
+$lang['NameOfRecipe'] = "Name of a recipe"; 
+$lang['CategoryOfRecipe'] = "Category"; 
+$lang['KitchenOfRecipe'] = "Cuisine"; 
+$lang['PortionsOfRecipe'] = "Portions"; 
+$lang['PortionsQttyOfRecipe'] = "units."; 
+$lang['IngredientsOfRecipe'] = "Components"; 
+$lang['TextOfRecipe'] = "Recipe"; 
+$lang['RecipeImgText'] = "Image:"; 
+$lang['errorDivName'] = "Type please a correct name, at least 5 characters long"; 
+$lang['errorDivPortions'] = "Type please a number of portions"; 
+$lang['errorDivIngredients'] = "Type please components, at least 25 characters"; 
+$lang['errorDivRecipeText'] = "Type please the way of cooking, at least 100 characters"; 
+
+
+//My recipes 
+$lang['MyRecipes'] = "My recipes"; 
+$lang['RecipesFilter'] = "Filter"; 
+$lang['YouHave'] = "You have"; 
+$lang['Recipes'] = "recipes"; 
+$lang['Total'] = "Total:"; 
+$lang['AddToFavorites'] = "Add to Favorites"; 
+$lang['Comments'] = "Comments"; 
+
+//View_recipe 
+$lang['SomeRecipe'] = "Recipe"; 
+$lang['AddedDateLabel'] = "Added"; 
+$lang['RatingLabel'] = "Rate"; 
+$lang['IngredientsText'] = "Components"; 
+$lang['RecipePhoto'] = "Photo of recipe"; 
+$lang['MainData'] = "Information about the recipe"; 
+$lang['FullSizePhotoDivTitle'] = "Full photo of recipe"; 
+
+//Comments 
+$lang['YourComment'] = "Leave a comment"; 
+$lang['SubmitCommentForm'] = "Leave a comment"; 
+$lang['WordCensorError'] = "<div class = \"Registraion_validator\" style=\"display:block;\">Please write your thoughts without offensive words</div>"; 
+$lang['DeleteRecipeLinkText'] = "Delete"; 
+$lang['WriteOn'] = "wrote"; 
+$lang['SendMessageToCommentAuthorText'] = "Message"; 
+$lang['errorDivComment'] = "Comments should be at least 5 characters long"; 
+
+//????????? 
+$lang['Favorites'] = "Favorite recipes"; 
+$lang['HisFavorites'] = "His (her) favorite recipes"; 
+$lang['Delete'] = "Delete"; 
+
+//Search 
+$lang['first_link'] = "Start"; 
+$lang['last_link'] = "End"; 
+$lang['Search'] = "Search"; 
+$lang['SearchOption'] = "Type of search"; 
+$lang['SearchUsers'] = " Search for people"; 
+$lang['SearchRecipies'] = " Search for recipes"; 
+$lang['SimpleSearch'] = "Simply search"; 
+$lang['SearchButton'] = "Search"; 
+$lang['SimpleSearchDescriptionUser'] = "In simple search you can type names and surnames"; 
+$lang['SimpleSearchDescriptionRecipies'] = "In simple search you can use the names of recipes"; 
+$lang['AdvancedSearch'] = "Advanced search"; 
+$lang['NoteSearch'] = "What does it mean ‘advanced search’?"; 
+$lang['NoteSearchAnswerUsers'] = "Advanced search gives a possibility to find friends by means of specific requests."; 
+$lang['NoteSearchAnswerRecipies'] = " Advanced search gives a possibility to find recipes by means of specific requests."; 
+$lang['Indigridients'] = "Ingredients:"; 
+$lang['RecipeText'] = "Cooking:"; 
+$lang['SearchUsersResultsText'] = "There were found {SearchResultsCount} persons"; 
+$lang['SearchRecipesResultsText'] = "There were found {SearchResultsCount} recipes"; 
+$lang['SearchDescriptionMain'] = "Enter a key-word for search"; 
+
+//Catalog 
+$lang['Catalog'] = "Catalogue of recipes"; 
+
+//Pager 
+$lang['Start'] = "Start"; 
+$lang['End'] = "End"; 
+
+//Forget password 
+$lang['ForgetPassword'] = "Password recovery"; 
+$lang['InformationForgetPassword'] = " Password recovery "; 
+$lang['NoteForgetPassword'] = "How do I restore my password?"; 
+$lang['NoteAnswerForgetPassword'] = "Type your E-mail, and you will receive a new one"; 
+$lang['SendForgetPassword'] = "Send a password"; 
+$lang['ForgetPasswordRequestSubject'] = "Request of a new password"; 
+$lang['ForgetPasswordRequestMessage'] = "<html> 
+<body> 
+<p>Dear {UserFullName}</p> 
+<br/> 
+<p>You have requested a new password for Oskrer.com web-site (http://www.Oskrer.com)</p> 
+<br/> 
+<p>To change the password follow this link {NewPasswordRequestUrl}</p> 
+<br/> 
+<p>If you did not request any password for your account on Oskrer.com web-site (http://www.Oskrer.com), just delete this letter </p> 
+<br/> 
+<p>The admimistration of My Taste.ru <a href=\"http://www.Oskrer.com\">http://www. Oskrer.com</a></p> 
+</body> 
+</html>"; 
+//Main 
+$lang['SimpleSearchRecipeMain'] = "Search recipes"; 
+$lang['Info1'] = "Do you enjoy eating tasty food?"; 
+$lang['Info2'] = "Are you keen on cooking?"; 
+$lang['Info3'] = "You enjoy communicating with other people, do not you?"; 
+$lang['Info4'] = "Would you like to find new friends?"; 
+$lang['Info5'] = "Join our club!"; 
+//$lang['LittleDescription'] = "Oskrer.com – is a site, that gives us a chance to find more recipes, and find people with common tastes or simply share favorite recipes. Join us!”; 
+$lang['LittleDescription'] = " Oskrer.com – is a social network connecting people with common tastes."; 
+$lang['population'] = "Total Culinary"; 
+$lang['number_of_recipes'] = "Total recipes"; 
+
+//???????????? 
+$lang['Users'] = "Cook"; 
+$lang['link_rate'] = "By rate"; 
+$lang['link_active'] = "By activity"; 
+$lang['link_newbes'] = "New"; 
+$lang['UsersPageDescrRate'] = "10 best cookers"; 
+$lang['UsersPageDescrActive'] = "10 most active cookers"; 
+$lang['UsersPageDescrNewbes'] = "10 new cookers "; 
+
+
+?>
