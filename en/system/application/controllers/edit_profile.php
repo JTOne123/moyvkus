7530f40bdde8b33108e6565c87ccedb43e4bf436
jthotbox <jthotbox@f427fdc5-e448-0410-9b95-ddf33fbbb69f>
2008-10-15 19:40:17 +0000

    локализация месяцов

---
+++ en/system/application/controllers/edit_profile.php
@@ -0,0 +1,362 @@
+<?php
+
+class Edit_Profile extends Controller {
+	
+	function Edit_Profile()
+	{
+		parent::Controller();
+		
+		$this->load->library('validation');
+		$this->load->library('user_managment');
+		$this->load->library('location');
+		
+		$this->load->helper('date');
+		$this->load->helper('form');
+		$this->load->helper('url');
+	}
+	
+	function _remap($method) {
+		//страницы, доступные без авторизации
+		$allowedPages = array('do_upload');
+		$pars = $this->uri->segment_array();
+		unset($pars[1]);
+		unset($pars[2]);
+		
+		
+		if (($method != null) &&
+				(($this->user_authorization->is_logged_in() !== false) ||  in_array($method, $allowedPages))) {
+			call_user_func_array(array($this, $method), $pars);
+		}
+		else
+			redirect('/login/', 'refresh');
+	}
+	
+	
+	function index()
+	{		
+		$data = $this->_load_headers();
+		
+		$data = $this->_load_resource($data);
+		
+		$data = $this->_data_bind($data);
+		
+		if($this->input->post('btnSave') == "true")
+		{
+			$this->update_user($this->user_authorization->get_loged_on_user_id());
+			if($this->update_password($this->user_authorization->get_loged_on_user_id()))
+				redirect('/profile/', 'refresh');
+			else
+				$data['ErrorNewPassword'] = $this->lang->line('ErrorNewPassword');
+		}
+		
+		$data['AvatarUploadError'] = '';
+		$data['body']= $this->parser->parse('edit_profile', $data);
+		
+		$this->parser->parse('main_tpl', $data);
+	}
+	
+	function _load_headers()
+	{
+		$data['title'] = $this->lang->line('EditProfile');
+		$data['keywords'] = $this->lang->line('keywords');
+		$data['description'] = $this->lang->line('description');
+		$data['baseurl'] = base_url();
+		$data['header'] = $this->load->view('header', $data, true);
+		$data['menu']=$this->Menu->buildmenu();
+		$data['login']='';
+		
+		return $data;
+	}
+	
+	function _load_resource($data)
+	{
+		// Локализация надписей
+		$data['Edit'] = $this->lang->line('Edit');
+		$data['Avatar'] = $this->lang->line('Avatar');
+		$data['MyFriendsHeader'] = $this->lang->line('MyFriendsHeader');
+		$data['MyData'] = $this->lang->line('MyData');
+		$data['FirstNameText'] = $this->lang->line('FirstNameText');
+		$data['LastNameText'] = $this->lang->line('LastNameText');
+		$data['SexText'] = $this->lang->line('SexText');
+		$data['BirthdayText'] = $this->lang->line('BirthdayText');
+		$data['LoctionText'] = $this->lang->line('LoctionText');
+		$data['WebSiteText'] = $this->lang->line('WebSiteText');
+		$data['InstantMessagerText'] = $this->lang->line('InstantMessagerText');
+		$data['ActivitiesText'] = $this->lang->line('ActivitiesText');
+		$data['InterestsText'] = $this->lang->line('InterestsText');
+		$data['AboutText'] = $this->lang->line('AboutText');
+		$data['MyRatingText'] = $this->lang->line('MyRatingText');
+		$data['MyRatingLevelText'] = $this->lang->line('MyRatingLevelText');
+		$data['MyBestRecipesText'] = $this->lang->line('MyBestRecipesText');
+		$data['MyInfo'] = $this->lang->line('MyInfo');
+		$data['MyRecipes'] = $this->lang->line('MyRecipes');
+		$data['Contacts'] = $this->lang->line('Contacts');
+		$data['LoginInformation'] = $this->lang->line('LoginInformation');
+		$data['NewEmailText'] = $this->lang->line('NewEmailText');
+		$data['OldPasswordText'] = $this->lang->line('OldPasswordText');
+		$data['NewPassword'] = $this->lang->line('NewPassword');
+		$data['ReNewPassword'] = $this->lang->line('ReNewPassword');
+		$data['SaveAllChanges'] = $this->lang->line('SaveAllChanges');
+		$data['Save'] = $this->lang->line('Save');
+		$data['Upload'] = $this->lang->line('Upload');
+		$data['Man'] = $this->lang->line('Man');
+		$data['Woman'] = $this->lang->line('Woman');
+		$data['Day'] = $this->lang->line('Day');
+		$data['Month'] = $this->lang->line('Month');
+		$data['Year'] = $this->lang->line('Year');
+		$data['Country'] = $this->lang->line('Country');
+		$data['Region'] = $this->lang->line('Region');
+		$data['City'] = $this->lang->line('City');
+		$data['MySettings'] = $this->lang->line('MySettings');
+		$data['Cancel'] = $this->lang->line('Cancel');
+		
+		return $data;
+	}
+	
+	
+	
+	function _data_bind($data)
+	{
+		$data['ProfileUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/profile';
+		$data['OldPasswordError'] = "none";
+		
+		$data['ErrorNewPassword'] = '';
+		
+		//Получение данных юзера и заполнение их
+		$users = $this->user_managment->GetUser($this->user_authorization->get_loged_on_user_id());
+		$user_data = $this->user_managment->GetUserData($this->user_authorization->get_loged_on_user_id());
+		
+		if($users != null)
+		{
+			$data['UserStatus'] = $users->first_name . ' ' . $users->last_name;
+			
+			$data['FirstName'] = $users->first_name;
+			$data['LastName'] = $users->last_name;
+			
+			if($users->sex == 0)
+			{
+				$data['txtSexManCHECKED'] = "checked";
+				$data['txtSexWomanCHECKED'] = "";	
+			}
+			else
+			{
+				$data['txtSexManCHECKED'] = "";
+				$data['txtSexWomanCHECKED'] = "checked";	
+			}
+			
+			
+			$day = array();
+			for($i=1;$i<=31;$i++)
+				$day[$i] = $i;
+			
+			$month = array(
+                                        '01'=>'January',
+                                        '02'=>'February',
+                                        '03'=>'March',
+                                        '04'=>'April',
+                                        '05'=>'May',
+                                        '06'=>'June',
+                                        '07' =>'July',
+                                        '08'=>'August',
+                                        '09'=>'September',
+                                        '10'=>'October',
+                                        '11'=>'November',
+                                        '12'=>'December'
+					);
+			
+			$current_year = mdate("%Y", time());
+			$year = array();
+			for($i=0;$i<=100;$i++)
+				$year[$current_year - $i] = $current_year - $i;
+			
+			$selectedDay = mdate("%d", mysql_to_unix($users->birthday));
+			$selectedMonth = mdate("%m", mysql_to_unix($users->birthday));
+			$selectedYear = mdate("%Y", mysql_to_unix($users->birthday));
+			
+			$data['SelectDay'] = form_dropdown('SelectDay', $day, $selectedDay);
+			$data['SelectMonth'] = form_dropdown('SelectMonth', $month, $selectedMonth);
+			$data['SelectYear'] = form_dropdown('SelectYear', $year, $selectedYear);
+			
+			$country=$this->location->GetCountryNames();
+			$nulled=array();
+			$data['SelectCountry'] = form_dropdown('SelectCountry',$country,'', "onChange='ajax_locator_country()'");
+			$data['SelectRegion'] = form_dropdown('SelectRegion',$nulled,'',"onChange='ajax_locator_region()'");
+			$data['SelectCity'] = form_dropdown('SelectCity');
+			
+			$data['Email'] = $users->email;
+		}
+		
+		if($user_data != null)
+		{
+			$data['WebSite'] = $user_data->website;
+			$data['InstantMessager'] = $user_data->phone;
+			$data['Activities'] = $user_data->activities;
+			$data['Interests'] = $user_data->interests;
+			$data['About'] = $user_data->about;
+			
+			if($user_data->avatar_name != null)
+				$data['AvatarUrl'] = '/uploads/user_avatars/'.$user_data->avatar_name;
+			else
+				$data['AvatarUrl'] = "../../images/noavatar.gif";
+			
+			
+		}
+		else
+		{
+			$data['WebSite'] = "";
+			$data['InstantMessager'] = "";
+			$data['Activities'] = "";
+			$data['Interests'] = "";
+			$data['About'] = "";
+			$data['AvatarUrl'] = "../../images/noavatar.gif";
+			
+		}
+		
+		return $data;
+	}
+	
+	//Работа с Ajax-загрузчиком городов START
+	function ajax_locator_country(){
+		
+		$countryID=$this->input->post('countryID');
+		
+		if(isset($countryID))
+		{
+			$region=$this->location->GetRegions($countryID);
+			$data['regions']=$region;
+			$this->load->view('locator_drop_down', $data);
+		}
+	}
+	
+	function ajax_locator_region(){
+		
+		$regionID=$this->input->post('regionID');
+		
+		if(isset($regionID))
+		{
+			$citys=$this->location->GetCitys($regionID);
+			$data['citys']=$citys;
+			$this->load->view('locator_drop_down', $data);
+		}
+		
+	}
+	//Работа с Ajax-загрузчиком городов END
+	
+	
+	function do_upload()
+	{
+		$data = $this->_load_headers();
+		$data = $this->_load_resource($data);
+		$data = $this->_data_bind($data);
+		
+		$this->load->library('image_lib');
+		
+		$config['upload_path'] = './uploads/user_avatars/stack';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '2048';
+		$this->load->library('upload', $config);
+		
+		if ( ! $this->upload->do_upload())
+		{
+			$data['AvatarUploadError'] = '<div class="Registraion_validator" style="display:block; height:50px;">' . $this->upload->display_errors() . '</div>';
+			$data['body']= $this->parser->parse('edit_profile', $data);
+			$this->parser->parse('main_tpl', $data);
+		}	
+		else //успешная загрузка
+		{
+			
+			$upl_arr=$this->upload->data();
+			
+			$config['image_library'] = 'GD2';
+			$config['new_image'] = './uploads/user_avatars/a_'.$this->user_authorization->get_loged_on_user_id().$upl_arr['file_ext'];
+			$config['source_image'] = './uploads/user_avatars/stack/'.$upl_arr['raw_name'].$upl_arr['file_ext'];
+			$config['quality'] = '90%';
+			$config['width'] = 300;
+			$config['height'] = 130;
+			$this->image_lib->initialize($config);
+			$this->image_lib->resize();
+			
+			$this->write_avatar_name_to_db($upl_arr['file_ext']);
+			unlink('./uploads/user_avatars/stack/'.$upl_arr['raw_name'].$upl_arr['file_ext']);
+			redirect('/edit_profile/', 'refresh');
+		}		
+	}
+	
+	function write_avatar_name_to_db($file_ext) //запись названия аватарки в базу
+	{
+		$user_id=$this->user_authorization->get_loged_on_user_id();
+		$this->user_managment->UpdateAvatar($user_id, $file_ext);
+	}
+	
+	
+	
+	function update_user($UserID)
+	{
+		$first_name = $this->input->post('txtFirstName');
+		$last_name = $this->input->post('txtLastName');
+		
+		$SelectDay = addslashes($this->input->post('SelectDay'));
+		$SelectMonth = addslashes($this->input->post('SelectMonth'));
+		$SelectYear = addslashes($this->input->post('SelectYear'));
+		
+		$SelectCountry = addslashes($this->input->post('SelectCountry'));
+		$SelectRegion = addslashes($this->input->post('SelectRegion'));
+		$SelectCity = addslashes($this->input->post('SelectCity'));
+		
+		$sexValue = addslashes($this->input->post('txtSex'));
+		
+		$phone = $this->input->post('txtPhone');
+		$website = $this->input->post('txtWebSite');
+		
+		$activities = $this->input->post('txtActivities');
+		$interests = $this->input->post('txtInterests');
+		$about = $this->input->post('txtAbout');
+		
+		$birthday =  "$SelectYear-$SelectMonth-$SelectDay";
+		
+		if($sexValue == "txtSexMan")
+			$sex = 0;
+		else
+			$sex = 1;
+		
+		$users = $this->user_managment->UpdateUser($UserID, $first_name, $last_name, $birthday, $sex, $SelectCity, $SelectRegion, $SelectCountry, $phone, $website, $activities, $interests, $about);
+	}
+	
+	function update_password($UserID)
+	{
+		$rules['txtNewPassword'] = "min_length[6]|max_length[21]|alpha_numeric";
+		$rules['txtOldPassword']="callback_check_old_password";
+		$this->validation->set_rules($rules);
+		
+		$fields['txtNewPassword'] = $this->lang->line('txtNewPassword');
+		$this->validation->set_fields($fields);
+		$this->validation->txtNewPassword_error;
+		
+		$old_password = $this->input->post('txtOldPassword');
+		$new_password = $this->input->post('txtNewPassword');
+		$txtReNewPassword = $this->input->post('txtReNewPassword');
+		
+		if($old_password!='')
+		{
+			if ($this->validation->run() == TRUE && $new_password == $txtReNewPassword) 
+			{				
+				$this->user_managment->NewPassword($UserID, $new_password);
+				return true;
+			}
+			else 
+				return false;
+		}
+		else 
+			return true;
+	}
+	
+	function check_old_password($old_password)
+	{
+		$ID=$this->user_authorization->get_loged_on_user_id();
+		
+		return $this->user_managment->IsPasswordValidByID($ID, $old_password);	
+	}
+	
+	
+}
+?>
---
+++ en/system/application/controllers/profile.php
@@ -0,0 +1,303 @@
+<?php
+
+class Profile extends Controller {
+	
+	function Profile()
+	{
+		parent::Controller();
+		
+		$this->load->library('validation');
+		
+		$this->load->library('user_managment');
+		$this->load->library('location');
+		$this->load->library('my_friends_lib');
+		$this->load->library('receipes_management');
+		
+		$this->load->helper('date');
+		$this->load->helper('typography');
+		
+		$this->load->model('Recommend');
+	}
+	
+	function _remap($method) {
+		//страницы, доступные без авторизации
+		$allowedPages = array();
+		$pars = $this->uri->segment_array();
+		unset($pars[1]);
+		unset($pars[2]);
+		
+		
+		if (($method != null) &&
+				(($this->user_authorization->is_logged_in() !== false) ||  in_array($method, $allowedPages))) {
+			call_user_func_array(array($this, $method), $pars);
+		}
+		else
+			redirect('/login/', 'refresh');
+	}
+	
+	function index()
+	{
+		
+		$data = $this->_load_headers();
+		
+		$data = $this->_load_resource($data);
+		
+		$data = $this->_data_bind($data);
+		
+		$data['body']= $this->parser->parse('profile', $data);
+		
+		$this->parser->parse('main_tpl', $data);
+	}
+	
+	
+	function _load_headers()
+	{
+		$data['title'] = $this->lang->line('title').' - '.$this->lang->line('Prifile');
+		$data['keywords'] = $this->lang->line('keywords');
+		$data['description'] = $this->lang->line('description');
+		$data['baseurl'] = base_url();
+		$data['header'] = $this->load->view('header', $data, true);
+		$data['menu']=$this->Menu->buildmenu();
+		$data['login']='';
+		
+		return $data;
+	}
+	
+	function _load_resource($data)
+	{
+		// Локализация надписей
+		$data['Avatar'] = $this->lang->line('Avatar');
+		$data['MyFriendsHeader'] = $this->lang->line('MyFriendsHeader');
+		$data['MyData'] = $this->lang->line('MyData');
+		$data['FirstNameText'] = $this->lang->line('FirstNameText');
+		$data['LastNameText'] = $this->lang->line('LastNameText');
+		$data['SexText'] = $this->lang->line('SexText');
+		$data['BirthdayText'] = $this->lang->line('BirthdayText');
+		$data['LoctionText'] = $this->lang->line('LoctionText');
+		$data['WebSiteText'] = $this->lang->line('WebSiteText');
+		$data['InstantMessagerText'] = $this->lang->line('InstantMessagerText');
+		$data['ActivitiesText'] = $this->lang->line('ActivitiesText');
+		$data['InterestsText'] = $this->lang->line('InterestsText');
+		$data['AboutText'] = $this->lang->line('AboutText');
+		$data['MyRatingText'] = $this->lang->line('MyRatingText');
+		$data['MyRatingLevelText'] = $this->lang->line('MyRatingLevelText');
+		$data['MyBestRecipesText'] = $this->lang->line('MyBestRecipesText');
+		$data['MyInfo'] = $this->lang->line('MyInfo');
+		$data['RecomendedRecipes'] = $this->lang->line('RecomendedRecipes');
+		$data['Contacts'] = $this->lang->line('Contacts');
+		$data['MyRatingTextHeader'] = $this->lang->line('MyRatingTextHeader');
+		$data['Friends'] = $this->lang->line('Friends');
+		$data['SendMessage'] = $this->lang->line('SendMessage');
+		$data['AddToFriends'] = $this->lang->line('AddToFriends');
+		$data['DeleteFromFriends'] = $this->lang->line('DeleteFromFriends');
+		
+		return $data;
+	}
+	
+	function _data_bind($data)
+	{
+		$user_id_from_uri = $this->uri->segment(3);
+		$user_id = $this->user_authorization->get_loged_on_user_id();
+		
+		if($user_id_from_uri == false) 
+		{
+			$user_id_from_uri = $user_id;
+			redirect('profile/id/'.$user_id, 'refresh');
+		}
+		
+		$user_id_to_view = $user_id;
+		
+		if($user_id_from_uri != $user_id_to_view)
+		{
+			if($this->user_managment->IsUserExists_by_id($user_id_from_uri) === true)
+				$user_id_to_view = $user_id_from_uri; 
+			else 
+				redirect('profile/id/'.$user_id_to_view, 'refresh');
+		}
+		
+		
+		//Показывать ли ссылки
+		if($user_id_to_view === $user_id)
+		{
+			$data['EditProfileUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/edit_profile';
+			$data['Edit'] = $this->lang->line('Edit');
+			
+			$data['SendMessageUrl'] = '';
+			$data['AddToFriendsUrl'] = '';
+			$data['DeleteFromFriendsUrl'] = '';
+			
+			$data['SendMessageShow'] = 'none';
+			$data['AddToFriendsShow'] = 'none';
+			$data['DeleteFromFriendsShow'] = 'none';
+			
+			$data['AddRecipeShow'] = '';
+			$data['AddRecipe'] = $this->lang->line('AddRecipe');
+			$data['AddRecipeUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/add_new_recipe';
+			
+			$data['MyRecipes'] = $this->lang->line('MyRecipes');
+			$data['MyRecipesUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/my_recipes';
+			
+			$data['Favorites'] = $this->lang->line('Favorites');
+			$data['FavoritesUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/favorites/id/' . $user_id_to_view;
+		}
+		else 
+		{
+			$data['AddRecipeShow'] = 'none';
+			
+			$data['MyRecipes'] = $this->lang->line('HisRecipes');
+			$data['MyRecipesUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/my_recipes/id/' . $user_id_to_view;
+			
+			$data['SendMessageShow'] = '';
+			$data['SendMessageUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/send_message/send_to/id/' . $user_id_to_view;
+			
+			$data['Favorites'] = $this->lang->line('HisFavorites');
+			$data['FavoritesUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/favorites/id/' . $user_id_to_view;
+			
+			//Проверка или просматриветься профиль друга
+			if($this->my_friends_lib->IsTheyFriends($user_id, $user_id_to_view))
+			{
+				$data['DeleteFromFriendsShow'] = '';
+				$data['DeleteFromFriendsUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/messagebox/type/delete_friend/friend_id/' . $user_id_to_view;
+				
+				$data['AddToFriendsShow'] = 'none';
+			}
+			else
+			{
+				$data['AddToFriendsShow'] = '';
+				$data['AddToFriendsUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/messagebox/type/add_friend/friend_id/' . $user_id_to_view;
+				
+				$data['DeleteFromFriendsShow'] = 'none';
+			}
+			
+			$data['EditProfileUrl']='';
+			$data['Edit'] = '';
+		}
+		
+		$month = array(
+                                    '01'=>'January',
+                                    '02'=>'February',
+                                    '03'=>'March',
+                                    '04'=>'April',
+                                    '05'=>'May',
+                                    '06'=>'June',
+                                    '07'=>'July',
+                                    '08'=>'August',
+                                    '09'=>'September',
+                                    '10'=>'October',
+                                    '11'=>'November',
+                                    '12'=>'December'
+				);
+		
+		//Получение данных юзера и заполнение их
+		$users = $this->user_managment->GetUser($user_id_to_view);
+		$user_data = $this->user_managment->GetUserData($user_id_to_view);
+		//var_dump($user_data);
+		if($users != null)
+		{
+			$data['UserStatus'] = $users->first_name . ' ' . $users->last_name;
+			
+			$data['FirstName'] = $users->first_name;
+			$data['LastName'] = $users->last_name;
+			$data['Sex'] = $this->GetSex($users->sex);
+			
+			$data['FriendsUrl'] = 'http://' . $_SERVER['HTTP_HOST'] . '/myfriends/id/' . $user_id_to_view;
+			
+			$day_string =  mdate("%d", mysql_to_unix($users->birthday));
+			$month_string =  mdate("%m", mysql_to_unix($users->birthday));
+			$year_string =  mdate("%Y", mysql_to_unix($users->birthday));
+			
+			$data['Birthday'] = $day_string .  ' ' . $month[$month_string] . ' ' . $year_string;
+			
+			if($users->city != null)
+			{
+				$city = $this->location->GetCity($users->city);
+				$city_name = $city->name;
+			}
+			else
+			{
+				$city_name = "";
+			}
+			if($users->country != null)	
+			{		
+				$country = $this->location->GetCountry($users->country);
+				$country_name = ', ' . $country->name;
+			}
+			else
+			{
+				$country_name = "";
+			}
+			
+			$data['Loction'] =  $city_name . $country_name;
+		}
+		
+		if($user_data != null)
+		{
+			$data['WebSite'] = $user_data->website;
+			$data['InstantMessager'] = $user_data->phone;
+			$data['Activities'] = auto_typography($user_data->activities);
+			$data['Interests'] = auto_typography($user_data->interests);
+			$data['About'] = auto_typography($user_data->about);
+			
+			$arr=$this->receipes_management->GetBestRecipe($user_id_to_view);
+			if($arr[0]['name'] !=='')
+			{
+			$data['MyBestRecipe'] = $arr[0]['name'];
+			$data['MyBestRecipeID'] = $arr[0]['id'];
+			}
+			else
+			{ 
+			$data['MyBestRecipe'] = '';
+			$data['MyBestRecipeID'] = '';
+			}
+			
+			$arr=$this->user_managment->GetUserRating($user_id_to_view);
+			$data['MyRating'] = $arr;
+			
+			$arr=$this->receipes_management->getuserrecipes($user_id_to_view, 0,5);
+			
+			$data['Recommend_recipes'] = $this->Recommend->Build($user_id_to_view);
+			
+			if($user_data->avatar_name != null)
+				$data['AvatarUrl'] = base_url().'/uploads/user_avatars/'.$user_data->avatar_name;
+			else
+				$data['AvatarUrl'] = base_url()."images/noavatar.gif";
+			
+			
+		}
+		else
+		{
+			$data['WebSite'] = "";
+			$data['InstantMessager'] = "";
+			$data['Activities'] = "";
+			$data['Interests'] = "";
+			$data['About'] = "";
+			$data['AvatarUrl'] = base_url()."images/noavatar.gif";
+			
+		}
+		
+		
+		return $data;
+	}
+	
+	
+	function GetSex($SexID)
+	{
+		if($SexID == null)
+			return "";
+		
+		switch($SexID)
+		{
+			case 0:
+				return $this->lang->line('Man');
+				break;
+			case 1: 
+				return $this->lang->line('Woman');
+				break;
+			default:
+				return "";
+				break;
+		}
+	}
+	
+}
+?>
