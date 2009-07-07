1db5291361b849296eb3aceaac9680e66b9a25c9
JTHotBox <JTHotBox@f427fdc5-e448-0410-9b95-ddf33fbbb69f>
2009-07-07 20:12:39 +0000

    обновил скрипты для таблицы юзер_дата

--- sql/database.sql
+++ sql/database.sql
@@ -88,17 +88,20 @@ CREATE TABLE `users` (
   `city` int(11) DEFAULT NULL,
   `region` int(11) DEFAULT NULL,
   `country` int(11) DEFAULT NULL,
+  `ci_sessions` text DEFAULT NULL,
   UNIQUE KEY `UserID` (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=cp1251;
 
 
-CREATE TABLE IF NOT EXISTS  `ci_sessions` (
-session_id varchar(40) DEFAULT '0' NOT NULL,
-ip_address varchar(16) DEFAULT '0' NOT NULL,
-user_agent varchar(50) NOT NULL,
-last_activity int(10) unsigned DEFAULT 0 NOT NULL,
-PRIMARY KEY (session_id)
-);
+DROP TABLE IF EXISTS `ci_sessions`;
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL default '0',
+  `ip_address` varchar(16) NOT NULL default '0',
+  `user_agent` varchar(50) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL default '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY  (`session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 /*Table structure for table `myfriends` */
 
